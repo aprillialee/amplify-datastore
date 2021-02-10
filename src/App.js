f03e29d3699e5a3bc8a622c55d85bdeef8a02819
aprillialee <aprilliapmlee@gmail.com>
2021-02-10 16:49:03 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+
+import logo from "./logo.svg";
+import "./App.css";
+import styled from "styled-components";
+
+import { DataStore } from "@aws-amplify/datastore";
+import { Post } from "./models";
+
+import Amplify from "@aws-amplify/core";
+import config from "./aws-exports";
+Amplify.configure(config);
+
+const initialState = { title: "" };
 
 function App() {
+  const [postState, updatePostState] = useState(initialState);
+  const [posts, updatePosts] = useState([]);
+
+  useEffect(() => {
+    fetchPosts();
+    const subscription = DataStore.observe(Post).subscribe(() => fetchPosts());
+    return () => subscription.unsubscribe();
+  });
+
+  function onChange(e) {
+    updatePostState({ title: e.target.value });
+  }
+
+  async function fetchPosts() {
+    const posts = await DataStore.query(Post);
+    updatePosts(posts);
+  }
+
+  async function createPost() {
+    if (!postState.title) return;
+    await DataStore.save(new Post({ ...postState }));
+    updatePostState(initialState);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <InputStyled onChange={onChange} name="title" value={postState.title} />
+      <ButtonStyled onClick={createPost}>Create Post</ButtonStyled>
+      {posts.map((post) => (
+        <p key={post.id}>{post.title}</p>
+      ))}
     </div>
   );
 }
 
 export default App;
+
+const InputStyled = styled.input``;
+
+const ButtonStyled = styled.button``;
---
+++ src/models/schema.js
@@ -0,0 +1,56 @@
+export const schema = {
+    "models": {
+        "Post": {
+            "name": "Post",
+            "fields": {
+                "id": {
+                    "name": "id",
+                    "isArray": false,
+                    "type": "ID",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "title": {
+                    "name": "title",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "rating": {
+                    "name": "rating",
+                    "isArray": false,
+                    "type": "Int",
+                    "isRequired": false,
+                    "attributes": []
+                },
+                "content": {
+                    "name": "content",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": false,
+                    "attributes": []
+                }
+            },
+            "syncable": true,
+            "pluralName": "Posts",
+            "attributes": [
+                {
+                    "type": "model",
+                    "properties": {}
+                }
+            ]
+        }
+    },
+    "enums": {
+        "PostStatus": {
+            "name": "PostStatus",
+            "values": [
+                "DRAFT",
+                "PUBLISHED"
+            ]
+        }
+    },
+    "nonModels": {},
+    "version": "53821ae4b32d5b17281c29ed65cae488"
+};
